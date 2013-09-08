4f9b50d510e131efe40d2cc0d7b47d2e87762f22
Zach Young <zachjyoung@gmail.com>
2013-09-08 17:56:21 -0400

    not seeing category in the view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130908212246) do
+ActiveRecord::Schema.define(version: 20130908214534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 20130908212246) do
     t.datetime "updated_at"
     t.string   "author"
     t.integer  "rating"
+  end
+
+  create_table "categories", force: true do |t|
+    t.string   "category"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "checkout", force: true do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,66 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#Should you load a csv?
+books = [
+  {
+    title: 'Galapagos',
+    author: 'Kurt Vonnegut',
+    rating: '4.5'
+  },
+  {
+    title: 'Douglas Adams',
+    author: "The Hitchhiker's Guide to the Galaxy",
+    rating: '4'
+  },
+  {
+    title: 'Glamorama',
+    author: 'Bret Easton Ellis',
+    rating: '1.5'
+  }
+]
+
+#use attributes for rails 4
+
+books.each do |attributes|
+  book = Book.where({
+    title: attributes[:title],
+    author: attributes[:author],
+    rating: attributes[:rating]
+    }).first
+
+  if book.nil?
+    book = Book.new(attributes)
+  else
+    book.attributes = attributes
+  end
+
+  book.save!
+end
+
+categorys = [
+  {
+    category: 'fiction',
+  },
+  {
+    category: 'sci-fi',
+  },
+  {
+    category: 'horror',
+  }
+]
+
+categorys.each do |attributes|
+  category = Category.where({
+    category: attributes[:category],
+    }).first
+
+  if category.nil?
+    category = Category.new(attributes)
+  else
+    category.attributes = attributes
+  end
+
+  category.save!
+end
